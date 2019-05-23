f54ab9e6b0bff1a1227a07a77ea3e7c1a2c50702
Brandon Barringer <brandon.barringer@CS4-BrandonBarringer-iMac.local>
2019-05-23 11:36:49 -0700

    [update] implements grass anim in main

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,10 @@
   ul
     list-style-type: none
     padding: 0
+  body, html 
+    width: 100%
+    height: 100%
+  #app
+    width: 100vw
+    height: 100vh
 </style>
--- src/components/GrassAnimation.vue
+++ src/components/GrassAnimation.vue
@@ -1,68 +1,71 @@
 <template>
-		<canvas class="grass" id="test"></canvas>
+		<canvas class="grass" id="grassAnim"></canvas>
 </template>
 
 <script>
 	export default {
 		name: 'GrassAnimation',
 		mounted() {
-			const canvas = document.getElementById("test"),
-			  ctx = canvas.getContext("2d"),
-			  stack = [],
-			  w = canvas.parentElement.clientWidth,
-			  h = canvas.parentElement.clientHeight;
+			const grassAnim = function() {
+				const canvas = document.getElementById("grassAnim"),
+				  ctx = canvas.getContext("2d"),
+				  stack = [],
+				  w = canvas.parentElement.clientWidth,
+				  h = canvas.parentElement.clientHeight;
 
-			const drawer = function() {
-			  ctx.fillStyle = "#ffffff";
-			  ctx.fillRect(0, 0, w, h);
-			  stack.forEach(function(el) {
-			    el();
-			  });
-			  requestAnimationFrame(drawer);
-			};
+				const drawer = function() {
+				  ctx.fillStyle = "#ffffff";
+				  ctx.fillRect(0, 0, w, h);
+				  stack.forEach(function(el) {
+				    el();
+				  });
+				  requestAnimationFrame(drawer);
+				};
 
-			const anim = function() {
-			  let x = 0;
-			  const speed = Math.random() * 7;
-			  const position = Math.random() * w - w / 2;
-			  const maxTall = Math.random() * 100 + 200;
-			  const maxSize = Math.random() * 10 + 5;
-			  const c = function(l, u) {
-			    return Math.round(Math.random() * (u || 255) + l || 0);
-			  };
-			  const color = "rgb(" + c(60, 10) + "," + c(201, 50) + "," + c(120, 50) + ")";
-			  
-			  return function() {
-			    const deviation = Math.cos(x / 30) * Math.min(x / 40, 50),
-			      tall = Math.min(x / 5, maxTall),
-			      size = Math.min(x / 50, maxSize);
+				const anim = function() {
+				  let x = 0;
+				  const speed = Math.random() * 7;
+				  const position = Math.random() * w - w / 2;
+				  const maxTall = Math.random() * 100 + 200;
+				  const maxSize = Math.random() * 10 + 5;
+				  const c = function(l, u) {
+				    return Math.round(Math.random() * (u || 255) + l || 0);
+				  };
+				  const color = "rgb(" + c(60, 10) + "," + c(201, 50) + "," + c(120, 50) + ")";
+				  
+				  return function() {
+				    const deviation = Math.cos(x / 30) * Math.min(x / 40, 50),
+				      tall = Math.min(x / 5, maxTall),
+				      size = Math.min(x / 50, maxSize);
 
-			    x += speed;
-			    ctx.save();
+				    x += speed;
+				    ctx.save();
 
-			    ctx.strokeWidth = 10;
-			    ctx.translate(w / 2 + position, h);
-			    ctx.fillStyle = color;
+				    ctx.strokeWidth = 10;
+				    ctx.translate(w / 2 + position, h);
+				    ctx.fillStyle = color;
 
-			    ctx.beginPath();
-			    ctx.lineTo(-size, 0);
-			    ctx.quadraticCurveTo(-size, -tall / 2, deviation, -tall);
-			    ctx.quadraticCurveTo(size, -tall / 2, size, 0);
-			    //ctx.closePath();
-			    ctx.fill();
-			    
-			    ctx.restore();
-			  };
-			};
+				    ctx.beginPath();
+				    ctx.lineTo(-size, 0);
+				    ctx.quadraticCurveTo(-size, -tall / 2, deviation, -tall);
+				    ctx.quadraticCurveTo(size, -tall / 2, size, 0);
+				    //ctx.closePath();
+				    ctx.fill();
+				    
+				    ctx.restore();
+				  };
+				};
 
-			for (var x = 0; x < 300; x++) {
-			  stack.push(anim());
+				for (var x = 0; x < 60; x++) {
+				  stack.push(anim());
+				}
+
+				canvas.width = w;
+				canvas.height = h;
+
+				drawer();
 			}
-
-			canvas.width = w;
-			canvas.height = h;
-
-			drawer();
+			grassAnim()
 		}
 	}
 		
