72e919557a9aa268a96b8ed7308b5e965c1343c1
Brandon Barringer <barringerbrandon@gmail.com>
2019-06-05 19:37:35 -0700

    removes max stats from data, adds constants file, updates max stats with constants

--- src/components/PokemonStatBlock.vue
+++ src/components/PokemonStatBlock.vue
@@ -1,8 +1,11 @@
 <template>
 	<section class="pokemon-stat-block">
 		<ul>
-			<li v-for="stat in data.stats">
-				<span>{{stat.stat.name}}: </span><span>{{stat.base_stat}} / {{stat.max_stat}}</span>
+			<li v-for="stat in data.stats" :key="stat.stat.name">
+				<span>{{stat.stat.name}}: </span>
+				<span>{{stat.base_stat}}</span>
+				<span v-if="stat.stat.name === 'total'"> / {{statMaxTotal}}</span>
+				<span v-else> / {{statMax}}</span>
 			</li>
 		</ul>
 	</section>
@@ -10,12 +13,19 @@
 
 <script>
 	import {mapState} from 'vuex'
+	import constants from '@/data/constants.json'
 
 	export default {
 		name: 'PokemonStatBlock',
 		computed: mapState({
 			data: state => state.PokemonList.activePokemon
 		}),
+		data() {
+			return {
+				statMax: constants.stats.max,
+				statMaxTotal: constants.stats.max_total
+			}
+		},
 		mounted() {
 			this.$store.dispatch('PokemonList/setActivePokemon', 1)
 		}
